3b4cfef8760406b309ff2a246f321d72754f6e62
Elias <e.engel@campus.tu-berlin.de>
2023-07-09 15:31:35 +0200

    added: test_readf

--- vorgabe/tests/test_readf.py
+++ vorgabe/tests/test_readf.py
@@ -23,7 +23,6 @@ class Test_Readf:
         fs = set_fil(name="fil1",inode=1,parent=0,parent_block=0,fs=fs)
         fs = set_data_block_with_string(block_num=0,string_data=LONG_DATA[:1024],parent_inode=1,parent_block_num=0,fs=fs)
         fs = set_data_block_with_string(block_num=1,string_data=LONG_DATA[1024:],parent_inode=1,parent_block_num=1,fs=fs)
-
         file_length = ctypes.c_int(0)
         retval = libc.fs_readf(ctypes.byref(fs), ctypes.c_char_p(bytes("/fil1","utf-8")),ctypes.byref(file_length))
 
@@ -60,4 +59,20 @@ class Test_Readf:
 
         assert file_length.value == 0
         assert retval == None
+        # test wrong path
+        file_length = ctypes.c_int(0)
+        retval = libc.fs_readf(ctypes.byref(fs), ctypes.c_char_p(bytes("fil1","utf-8")),ctypes.byref(file_length))
 
+        assert file_length.value == 0
+        assert retval == None
+
+    # !useless: but iam to bored to fix it -> check if BLOCK is null char
+    def test_readf_NULL_in_string(self):
+        fs = setup(5)
+        teststring = "Hallo NULL der string soll hier aber noch weitergehen!"
+        fs = set_fil(name="fil1",inode=1,parent=0,parent_block=0,fs=fs)
+        fs = set_data_block_with_string(block_num=0,string_data=teststring,parent_inode=1,parent_block_num=0,fs=fs)
+        file_length = ctypes.c_int(0)
+        retval = libc.fs_readf(ctypes.byref(fs), ctypes.c_char_p(bytes("/fil1","utf-8")),ctypes.byref(file_length))
+        assert file_length.value == len(teststring)
+        assert retval.decode("utf-8") == teststring
